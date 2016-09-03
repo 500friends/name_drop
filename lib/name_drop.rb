f78f9f2785fabe3d3370e2c3e4178dba337ecf81
Paul Pettengill <ppettengill@merkleinc.com>
2016-09-02 23:28:09 -0700

    Configuring Gem

--- lib/name_drop.rb
+++ lib/name_drop.rb
@@ -1,5 +1,11 @@
 require 'name_drop/version'
 
 module NameDrop
-  # Your code goes here...
+  class << self
+    attr_reader :config
+
+    def config=(config)
+      @config ||= Struct.new(*config.keys).new(*config.values)
+    end
+  end
 end
