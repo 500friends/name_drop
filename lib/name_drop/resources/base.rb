ccca6390c547f76ac85be0ef3a4afac24f01f47d
Paul Pettengill <ppettengill@merkleinc.com>
2016-09-07 12:25:35 -0700

    Setting parsed response to with_indifferent access

--- lib/name_drop/resources/base.rb
+++ lib/name_drop/resources/base.rb
@@ -1,6 +1,6 @@
-require 'active_support'
 require 'active_support/inflector'
 require 'active_support/core_ext/object'
+require 'active_support/core_ext/hash/indifferent_access'
 
 module NameDrop
   module Resources
@@ -10,7 +10,7 @@ module NameDrop
 
       def initialize(client, attributes = {})
         @client = client
-        @attributes = attributes
+        @attributes = attributes.with_indifferent_access
         @errors = []
       end
 
@@ -35,7 +35,7 @@ module NameDrop
       end
 
       def save
-        path = new_record? ? endpoint : "#{endpoint}/#{attributes['id']}"
+        path = new_record? ? endpoint : "#{endpoint}/#{attributes[:id]}"
         response = client.send(persistence_action, path, attributes)
         if response[response_key].present?
           self.attributes = response[response_key]
@@ -47,7 +47,7 @@ module NameDrop
       end
 
       def destroy(params = {})
-        client.delete("#{endpoint(params)}/#{attributes['id']}")
+        client.delete("#{endpoint(params)}/#{attributes[:id]}")
       end
 
       def self.response_key
@@ -67,7 +67,7 @@ module NameDrop
       end
 
       def new_record?
-        !attributes['id']
+        !attributes[:id]
       end
     end
   end
