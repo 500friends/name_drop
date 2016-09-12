4ca95ba21caa38376648525c868a29110c36b59a
Paul Pettengill <ppettengill@merkleinc.com>
2016-09-12 12:17:45 -0700

    Adding more YARD documentation

--- lib/name_drop/client.rb
+++ lib/name_drop/client.rb
@@ -1,53 +1,92 @@
 require 'rest-client'
 
 # Namespace for classes and modules that handle interaction with Mention API
+#
 # @since 0.1.0
 module NameDrop
   # Class that encapsulates calls to Mention API
+  #
   # @since 0.1.0
   class Client
+    # Initializes new NameDrop::Client object
     def initialize(*)
     end
 
+    # @!group Resources
+
+    # Allows access through client.alerts
+    #
     # @return [NameDrop::Resources::Alert]
+    # @example accessing alerts for client
+    #   client = NameDrop::Client.new
+    #   alerts = client.alerts.all
     def alerts
       Resources::BaseFactory.new(self, 'Alert')
     end
 
+    # Allows access through client.mentions
+    #
     # @return [NameDrop::Resources::Mention]
+    # @example accessing mentions for client
+    #   client = NameDrop::Client.new
+    #   mentions = client.mentions.all
     def mentions
       Resources::BaseFactory.new(self, 'Mention')
     end
 
+    # Allows access through client.shares
+    #
     # @return [NameDrop::Resources::Share]
+    # @example accessing shares for client
+    #   client = NameDrop::Client.new
+    #   shares = client.shares.all
     def shares
       Resources::BaseFactory.new(self, 'Share')
     end
 
+    # @!endgroup
+    # @!group HTTP Requests
+
+    # Makes GET Request through RestClient::Request.execute
+    #
     # @param [String] endpoint
+    # @return [Hash] encapsulates Mention API response of GET request
     def get(endpoint)
       request(:get, endpoint)
     end
 
+    # Makes POST Request through RestClient::Request.execute
+    #
     # @param [String] endpoint
     # @param [Hash] attributes
+    # @return [Hash] encapsulates Mention API response of POST request
     def post(endpoint, attributes)
       request(:post, endpoint, attributes)
     end
 
+    # Makes PUT Request through RestClient::Request.execute
+    #
     # @param [String] endpoint
     # @param [Hash] attributes
+    # @return [Hash] encapsulates Mention API response of PUT request
     def put(endpoint, attributes)
       request(:put, endpoint, attributes)
     end
 
+    # Makes DELETE Request through RestClient::Request.execute
+    #
     # @param [String] endpoint
+    # @return [Hash] encapsulates Mention API response of DELETE request
     def delete(endpoint)
       request(:delete, endpoint)
     end
 
+    # @!endgroup
+
     private
 
+    # Calls RestClient::Request.execute with request hash, then parses response with JSON unless response is empty
+    #
     # @param [Symbol] method
     # @param [String] endpoint
     # @param [Hash] attributes
@@ -60,6 +99,8 @@ module NameDrop
       raise NameDrop::Error.new(error_message(method), JSON.parse(err.response).with_indifferent_access)
     end
 
+    # Sets request hash for API including attributes if present
+    #
     # @param [Symbol] method
     # @param [String] endpoint
     # @param [Hash] attributes
@@ -70,6 +111,8 @@ module NameDrop
       request_hash
     end
 
+    # Sets error message
+    #
     # @param [Symbol] method
     # @return [String] error message
     def error_message(method)
@@ -81,6 +124,8 @@ module NameDrop
       { get: 'retrieving', post: 'creating', put: 'updating', delete: 'deleting' }
     end
 
+    # Sets headers for HTTP request including Mention access_token
+    #
     # @return [Hash] headers HTTP request
     def headers
       @headers ||= { 'Content-Type' => 'application/json',
@@ -88,6 +133,8 @@ module NameDrop
                      'Authorization' => "Bearer #{NameDrop.configuration.access_token}" }
     end
 
+    # Builds URL string for Mention API Request including Mention account_id
+    #
     # @param [String] endpoint
     # @return [String] URL
     def request_url(endpoint)
--- lib/name_drop/configuration.rb
+++ lib/name_drop/configuration.rb
@@ -1,7 +1,9 @@
 # Namespace for classes and modules that handle interaction with Mention API
+#
 # @since 0.1.0
 module NameDrop
   # Holds configuration for Mention API
+  #
   # @since 0.1.0
   class Configuration
     # @!attribute account_id
@@ -10,6 +12,9 @@ module NameDrop
     #   @return [String] Mention Application Access Token
     attr_accessor :account_id, :access_token
 
+    # Initializes new NameDrop::Configuration object
+    #
+    # Sets default account_id and access_token
     def initialize
       @account_id = 'NotARealAccountID'
       @access_token = 'NotARealAccessToken'
@@ -17,14 +22,28 @@ module NameDrop
   end
 
   class << self
+    # Returns existing configuration, or a new configuration
+    #
+    # @return [NameDrop::Configuration] either predefined or a new instance
     def configuration
       @configuration ||= Configuration.new
     end
 
+    # Sets configuration for NameDrop
+    #
+    # @param [NameDrop::Configuration] config
     def configuration=(config)
       @configuration = config
     end
 
+    # Allows configuration of NameDrop through block
+    #
+    # @example Set account_id and access_token
+    #   NameDrop.configure do |config|
+    #     config.account_id = ENV['MENTION_API_ACCOUNT_ID']
+    #     config.access_token = ENV['MENTION_API_ACCESS_TOKEN']
+    #   end
+    # @yieldreturn [NameDrop::Configuration] configuration
     def configure
       yield(configuration)
     end
