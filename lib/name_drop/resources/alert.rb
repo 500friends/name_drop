a2c9fedcacc0d4d6a12ecf4a0615fdf465a29927
Adam Bedford <abedford@merkleinc.com>
2016-09-12 17:11:48 -0700

    Remove endpoint methods from Alert

--- lib/name_drop/resources/alert.rb
+++ lib/name_drop/resources/alert.rb
@@ -17,22 +17,6 @@ module NameDrop
       def destroy
         raise NotImplementedError, 'You cannot delete an Alert directly'
       end
-
-      # Sets suffix of Mention API call
-      #
-      # @see NameDrop::Resources::Alert.endpoint
-      # @return [String] 'alerts'
-      def endpoint
-        self.class.endpoint
-      end
-
-      # Sets suffix of Mention API call
-      #
-      # @param [Hash] _params (unused) for Alerts inherited from Base
-      # @return [String] 'alerts'
-      def self.endpoint(_params = {})
-        'alerts'
-      end
     end
   end
 end
